1f3385b78242453c213dbd2ef5b9ea186d77dc29
NassimSahib <140825067+NassimSahib@users.noreply.github.com>
2026-05-21 14:20:21 +0200

    Add: depth-of-binary-tree - submission-3

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-3.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+
+        int maxLeft = dfs(root->left);
+        int maxRight = dfs(root->right);
+
+        return 1 + std::max(maxLeft, maxRight);
+
+        
+    }
+
+    int dfs(TreeNode* node) {
+        if (!node) return 0;
+
+        int maxLeft =  dfs(node->left);
+        int maxRight = dfs(node->right);
+
+        return 1 + std::max(maxLeft,maxRight);
+
+    }
+};
