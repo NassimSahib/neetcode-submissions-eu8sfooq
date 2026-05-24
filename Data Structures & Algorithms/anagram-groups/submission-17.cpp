5b7cdefba00a95e6908df3dbd9dde64828ab9fdc
NassimSahib <140825067+NassimSahib@users.noreply.github.com>
2026-05-24 18:29:46 +0200

    Add: anagram-groups - submission-17

---
+++ Data Structures & Algorithms/anagram-groups/submission-17.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string,vector<string>> dict;
+
+        for (const string& s: strs) {
+            string base = s;
+            sort(base.begin(),base.end());
+            dict[base].push_back(s);
+        }
+
+        for (auto& [base,anagrams]: dict) {
+            res.push_back(anagrams);
+        }
+
+        return res;
+    }
+};
