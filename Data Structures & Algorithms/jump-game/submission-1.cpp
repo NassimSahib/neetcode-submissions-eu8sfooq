47664ce923dc7750fe5323994da7458df4985569
Nassim Sahib <140825067+NassimSahib@users.noreply.github.com>
2026-09-05 16:21:16 +0200

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size() - 1;
+
+        for (int i = goal; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0 ? true: false;
+    }
+};
